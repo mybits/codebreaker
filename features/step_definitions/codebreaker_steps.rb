81ed9c8a5e10b01e51ee53fc6748e10a28f68793
PaulaEs <paulaproxy@gmail.com>
2014-03-21 13:59:34 +0000

    Correcting regexp.

--- features/step_definitions/codebreaker_steps.rb
+++ features/step_definitions/codebreaker_steps.rb
@@ -22,7 +22,7 @@ When(/^I start a new game$/) do
 end
 
 
-Then(/^I should see "([^"]*)"$/) do |message|
+Then(/^I should see '(.*?)'$/) do |message|
   output.messages.should include(message)
 end
 
