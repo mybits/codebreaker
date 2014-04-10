8cde8be18326a4005b5db04c3ee594cf99f9a793
PaulaEs <paulaproxy@gmail.com>
2014-04-10 01:37:10 +0100

    Extracting exact match count method.

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -12,22 +12,16 @@ module Codebreaker
     end
 
     def guess(guess)
-      exact_match_count = 0
       number_match_count = 0
       (0..3).each do |index|
         if exact_match?(guess, index)
           exact_match_count += 1
         end
       end
-      (0..3).each do |index|
-        if number_match?(guess, index)
-          number_match_count += 1
-        end
-      end
+
       @output.puts '+' *exact_match_count + '-' *number_match_count
+
     end
-
-
 
 
     def exact_match?(guess, index)
@@ -36,6 +30,16 @@ module Codebreaker
 
     def number_match?(guess, index)
       @secret.include?(guess[index]) && !exact_match?(guess, index)
+    end
+
+    def exact_match_count
+      exact_match_count = 0
+      (0..3).each do |index|
+        if exact_match?(guess, index)
+        exact_match_count += 1
+        end
+      end
+      exact_match_count
     end
 
   end
