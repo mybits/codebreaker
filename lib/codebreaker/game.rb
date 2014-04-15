7373e77128709ee13d82e0bb5ac6a46cfdbd2676
PaulaEs <paulaproxy@gmail.com>
2014-04-15 23:06:17 +0100

    Completing extracting Marker class.

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -12,38 +12,9 @@ module Codebreaker
     end
 
     def guess(guess)
-      marker = Marker.new(@secret)
+      marker = Marker.new(@secret, guess)
       @output.puts '+'*marker.exact_match_count(guess) + '-'*marker.number_match_count(guess)
     end
-
-    class Marker
-
-      def initialize(secret)
-        @secret = secret
-      end
-
-
-      def exact_match_count(guess)
-        (0..3).inject(0) do |count, index|
-          count + (exact_match?(guess, index) ? 1 : 0)
-        end
-      end
-
-      def number_match_count(guess)
-        (0..3).inject(0) do |count, index|
-          count + (number_match?(guess, index) ? 1 : 0)
-        end
-      end
-
-      def exact_match?(guess, index)
-        guess[index] == @secret[index]
-      end
-
-      def number_match?(guess, index)
-        @secret.include?(guess[index]) && !exact_match?(guess, index)
-      end
-    end
-
 
   end
 end
