dcd24b9fc20b7f52231f51395764d5b0b365ca09
PaulaEs <paulaproxy@gmail.com>
2014-04-24 23:54:24 +0100

    Implementing match duplicated in guess.

--- lib/codebreaker/marker.rb
+++ lib/codebreaker/marker.rb
@@ -12,9 +12,19 @@ class Marker
   end
 
   def number_match_count
-    (0..3).inject(0) do |count, index|
-      count + (number_match?(index) ? 1 : 0)
+    total_match_count - exact_match_count
+  end
+
+  def total_match_count
+    count = 0
+    secret = @secret.split('')
+    @guess.split('').map do |n|
+      if secret.include?(n)
+        secret.delete_at(secret.index(n))
+        count += 1
+      end
     end
+    count
   end
 
   def exact_match?(index)
@@ -24,4 +34,8 @@ class Marker
   def number_match?(index)
     @secret.include?(@guess[index]) && !exact_match?(index)
   end
+
+
+
 end
+
